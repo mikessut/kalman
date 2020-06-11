f1b407cafffae55c32f26035fd09a36b2b49d185
Mike Sutton <mikesutton@gmail.com>
2020-06-10 17:03:37 -0700

    big bug; gyro not gyro data in stream; simplify unit q

--- orient/phone_test.py
+++ orient/phone_test.py
@@ -12,7 +12,18 @@ try:
     import fixgw.netfix as netfix
 except ModuleNotFoundError:
     pass
+import threading
+import time
 
+
+codes = {1: 'gps',
+         3: 'accel',
+         5: 'mag',
+         4: 'gyro',
+         6: 'pos',
+         7: 'unkn',
+         8: 'unkn2',
+         }
 
 
 class FIXGWInterface:
@@ -28,50 +39,66 @@ class FIXGWInterface:
         self.client.writeValue("HEAD", head360(-head*np.pi/180))
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-s.bind(('', 5555))
+
+def run_func(done, k, fixgw, raw_log):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    s.bind(('', 5555))
+
+    while not done.is_set():
+
+        msg, adr = s.recvfrom(8192)
+        raw_log.append((time.time(), msg))
+        cols = msg.split(b',')
+
+        data = {}
+        for c, name in codes.items():
+            data[name] = []
+
+        for n in range(1, len(cols), 4):
+            c = int(cols[n])
+            data[codes[c]] = np.array([float(x) for x in cols[n+1:n+4]])
+
+        k.predict()
+
+        #import pdb; pdb.set_trace()
+        if len(data['accel']) == 3:
+            #accels[0] += 1
+            k.update_accel(data['accel'])
+
+        if len(data['gyro']) == 3:
+            data['gyro'][0] += 2*np.pi/180
+            k.update_gyro(data['gyro'])
+
+        if len(data['mag']) == 3:
+            k.update_mag(data['mag'])
+
+        #print(k.quaternion().euler_angles()*180/np.pi)
+        euler_angles = k.quaternion().euler_angles()*180/np.pi
+        print(f"{euler_angles[0]:7.1f}{euler_angles[1]:7.1f}{euler_angles[2]:7.1f}  {k.wb[0]*180/np.pi:6.2f}{k.wb[1]*180/np.pi:6.2f}{k.wb[2]*180/np.pi:6.2f} {k.ab[0]:6.2f}{k.ab[1]:6.2f}{k.ab[2]:6.2f} {k.w[0]*180/np.pi:6.2f}{k.w[1]*180/np.pi:6.2f}{k.w[2]*180/np.pi:6.2f}")
+        d = np.diag(k.P)
+        #print(d[7:10])
+        fixgw.update(k.quaternion())
+
+
 
 k = qEKF.qEKF()
 fixgw = FIXGWInterface()
+done = threading.Event()
+raw_log = []
+done.clear()
 
+thread = threading.Thread(target=run_func, args=(done, k, fixgw, raw_log))
+thread.start()
 
-done = False
-while not done:
+while True:
+    tmp = input()
+    if tmp == 'q':
+        done.set()
+        break
 
-    msg, adr = s.recvfrom(8192)
-    cols = msg.split(b',')
-
-    accels = np.array([float(x) for x in cols[2:5]])
-    gyros = np.array([float(x) for x in cols[6:9]])
-    mags = np.array([float(x) for x in cols[10:13]])
-
-    k.predict()
-
-    #import pdb; pdb.set_trace()
-    if len(accels) == 3:
-        #accels[0] += 1
-        k.update_accel(accels)
-
-    if len(gyros) == 3:
-        gyros[0] += 2*np.pi/180
-        k.update_gyro(gyros)
-
-    if len(mags) == 3:
-        k.update_mag(mags)
-    #k.log.mag(mags)
-
-    #print(k.quaternion().euler_angles()*180/np.pi)
-    euler_angles = k.quaternion().euler_angles()*180/np.pi
-    print(f"{euler_angles[0]:7.1f}{euler_angles[1]:7.1f}{euler_angles[2]:7.1f}  {k.wb[0]*180/np.pi:6.2f}{k.wb[1]*180/np.pi:6.2f}{k.wb[2]*180/np.pi:6.2f} {k.ab[0]:6.2f}{k.ab[1]:6.2f}{k.ab[2]:6.2f} {k.w[0]*180/np.pi:6.2f}{k.w[1]*180/np.pi:6.2f}{k.w[2]*180/np.pi:6.2f}")
-    d = np.diag(k.P)
-    #print(d[7:10])
-    fixgw.update(k.quaternion())
-
-    #if msvcrt.kbhit():
-    #    done = True
-
-
+import pdb; pdb.set_trace()
 f, ax = plt.subplots(4,1, sharex=True)
 ax[0].plot(*k.log.get_eulers())
 ax[0].set_ylabel('Eulers (deg)')
