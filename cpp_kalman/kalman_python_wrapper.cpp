6bde8a452ff7e0dd1c5f71b19eacce09efab0a76
Mike Sutton <mikesutton@gmail.com>
2020-11-16 17:48:07 -0700

    Ported to cpp; wrappers

---
+++ cpp_kalman/kalman_python_wrapper.cpp
@@ -0,0 +1,33 @@
+
+#include "kalman.h"
+
+#define LIBEXPORT extern "C" __attribute__((__visibility__("default")))
+
+
+LIBEXPORT void* CreateKalmanClass() {
+    return new Kalman;
+}
+
+
+LIBEXPORT void* PrintState(void* ptr) {
+    Kalman* ref = reinterpret_cast<Kalman*>(ptr);
+    ref->printState();
+}
+
+
+LIBEXPORT void predict(void *ptr, float dt) {
+    Kalman* ref = reinterpret_cast<Kalman*>(ptr);
+    ref->predict(dt);
+}
+
+
+LIBEXPORT void update_accel(void *ptr, float x, float y, float z) {
+    Kalman* ref = reinterpret_cast<Kalman*>(ptr);
+    ref->update_accel(Vector3f(x, y, z));
+}
+
+
+LIBEXPORT void* state_ptr(void *ptr) {
+    Kalman* ref = reinterpret_cast<Kalman*>(ptr);
+    ref->x.data();
+}
